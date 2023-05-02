86505fe78137cd68019fdfc159bfc6675a7dbb84
Mayowa Sodipo <67042714+Mayorhack@users.noreply.github.com>
2023-05-02 11:22:18 +0100

    fix: build errors

--- src/pages/dashboard/upload.jsx
+++ src/pages/dashboard/upload.jsx
@@ -1,19 +1,23 @@
-import Button from '@/components/Button'
-import FormInput from '@/components/forms/FormInput'
-import MyDatePicker from '@/components/forms/MyDatePicker'
-import uploadIcon from '@/assets/Upload icon.png'
-import Image from 'next/image'
-import Card from '@/components/Card'
+import Button from "@/components/Button";
+import FormInput from "@/components/forms/FormInput";
+import MyDatePicker from "@/components/forms/MyDatePicker";
+import uploadIcon from "@/assets/Upload icon.png";
+import Image from "next/image";
+import Card from "@/components/Card";
 
 const Upload = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <Card className=" p-8 mt-8">
       <form action="" className="flex flex-col md:flex-row items-center">
         <div className="w-full p-5 space-y-6">
-          <FormInput label={'Document Name'} />
-          <MyDatePicker />
-          <FormInput label={'Staff Name'} />
-          <FormInput label={'Email Address'} />
+          <FormInput label={"Document Name"} />
+          <MyDatePicker
+            startDate={startData}
+            onChange={() => setStartDate(new Date())}
+          />
+          <FormInput label={"Staff Name"} />
+          <FormInput label={"Email Address"} />
         </div>
         <div className="w-full p-5 ">
           <div className="border-2 border-dashed rounded-3xl min-h-[260px] flex justify-center items-center flex-col">
@@ -27,7 +31,7 @@ const Upload = () => {
         <Button>Upload</Button>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export default Upload
+export default Upload;
--- src/utilities/handleEvents.js
+++
@@ -1,5 +0,0 @@
-export const handleChange = function (e, action) {
-  const name = e.target.name;
-  const value = e.target.value;
-  action();
-};
