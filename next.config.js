aad489935428c513f37e05cc530afbb5015fa12a
Mayowa Sodipo <sodipomayowa7@gmail.com>
2023-06-30 11:42:57 +0100

    fix:updates3

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  distDir: 'dist',
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
+  // trailingSlash: true,
+  // distDir: 'dist',
 }
 
 module.exports = nextConfig
